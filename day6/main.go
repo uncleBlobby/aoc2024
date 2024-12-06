242592f7b9231c2fa3f0e98f41dbca3cf95b5382
Dustin Christianson <dc.christianson@gmail.com>
2024-12-06 13:57:39 -0600

    day 6 part 1 visualization part 2 stuck

--- day6/main.go
+++ day6/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"image/color"
 	"log"
 	"strings"
+	"time"
 
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/vector"
 	daily "github.com/uncleBlobby/aoc2024/internal"
 )
 
@@ -19,15 +24,63 @@ type Direction struct {
 	Y int
 }
 
+type StepInformation struct {
+	Position  Position
+	Direction Direction
+}
+
 type Guard struct {
 	Position Position
-	History  []Position
+	History  []StepInformation
 	Facing   Direction
 }
 
 // type WorldMap struct {
 // 	Positions map[Position]string
 // }
+
+type Game struct {
+	WorldMap      map[Position]string
+	Guard         Guard
+	Clone         Guard
+	worldBoundary Position
+	currentFrame  int
+	DrawnPath     []Position
+}
+
+func (g *Game) Update() error {
+	g.currentFrame += 1
+	time.Sleep(66 * time.Millisecond)
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello, world!")
+
+	vector.DrawFilledRect(screen, 100, 100, 10, 10, color.RGBA{255, 0, 0, 255}, false)
+
+	for position, char := range g.WorldMap {
+		if char == "." {
+			vector.DrawFilledRect(screen, float32(position.X)*2, float32(position.Y)*2, 2, 2, color.RGBA{239, 239, 240, 1}, false)
+		}
+		if char == "#" {
+			vector.DrawFilledRect(screen, float32(position.X)*2, float32(position.Y)*2, 2, 2, color.RGBA{255, 148, 112, 1}, false)
+		}
+	}
+
+	vector.DrawFilledRect(screen, float32(g.Guard.History[g.currentFrame].Position.X)*2, float32(g.Guard.History[g.currentFrame].Position.Y)*2, 2, 2, color.RGBA{45, 85, 255, 255}, false)
+
+	g.DrawnPath = append(g.DrawnPath, g.Guard.History[g.currentFrame].Position)
+
+	for _, position := range g.DrawnPath {
+		vector.DrawFilledRect(screen, float32(position.X)*2, float32(position.Y)*2, 2, 2, color.RGBA{45, 85, 255, 150}, false)
+	}
+
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
 
 func main() {
 	fmt.Println("hello, aoc2024 day6!")
@@ -41,6 +94,10 @@ func main() {
 	lineCount := 0
 
 	worldBoundary := Position{}
+
+	game := &Game{
+		currentFrame: 1,
+	}
 
 	world := map[Position]string{}
 	guard := Guard{}
@@ -61,7 +118,10 @@ func main() {
 					X: 0,
 					Y: -1,
 				}
-				guard.History = append(guard.History, guard.Position)
+				guard.History = append(guard.History, StepInformation{
+					Position:  guard.Position,
+					Direction: guard.Facing,
+				})
 				thisPosition := Position{
 					X: ind,
 					Y: lineCount,
@@ -79,15 +139,73 @@ func main() {
 		lineCount += 1
 		worldBoundary.X = len(chars)
 		worldBoundary.Y = lineCount
+		game.worldBoundary = worldBoundary
 	}
 
-	for guard.InBounds(worldBoundary) {
-		guard.Travel(world)
+	game.WorldMap = world
+	game.Guard = guard
+
+	newObstacles := []Position{}
+
+	for game.Guard.InBounds(game.worldBoundary) {
+		log.Printf("%d percent complete...", len(game.Guard.History)+1/5067)
+		game.Clone = game.Guard
+		anchor := StepInformation{
+			guard.Position,
+			guard.Facing,
+		}
+		game.Clone.Facing = guard.Facing.Rotate90()
+		for game.Clone.InBounds(game.worldBoundary) {
+			//fmt.Printf("clone guard %v\n", guard)
+			game.Clone.Travel(game.WorldMap)
+		}
+
+		for _, historyStep := range game.Clone.History {
+			//log.Printf("checking path steps %v\n", historyStep)
+			if historyStep == anchor {
+				// found a loop if we put obstacle in facing direction of anchor position
+				log.Printf("found new obstacle position")
+				newObstacles = append(newObstacles, Position{anchor.Position.X + anchor.Direction.X, anchor.Position.Y + anchor.Direction.Y})
+				break
+			}
+		}
+
+		game.Guard.Travel(game.WorldMap)
+		//time.Sleep(1 * time.Second)
 	}
 
-	path := guard.FindUniquePathPositions()
+	path := game.Guard.FindUniquePathPositions()
 
 	fmt.Printf("Guard visits %d unique positions before leaving map\n", len(path)+1)
+
+	for i := 0; i < len(newObstacles); i++ {
+		if contains(newObstacles[i+1:], newObstacles[i]) {
+			newObstacles = remove(newObstacles, i)
+			i--
+		}
+	}
+
+	fmt.Printf("Can add obstacles at %d positions", len(newObstacles))
+
+	ebiten.SetWindowSize(1000, 1000)
+	ebiten.SetWindowTitle("Hello, World")
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func contains(p []Position, t Position) bool {
+	for _, a := range p {
+		if a == t {
+			return true
+		}
+	}
+	return false
+}
+
+func remove(slice []Position, idx int) []Position {
+	return append(slice[:idx], slice[idx+1:]...)
 }
 
 func (g *Guard) InBounds(boundary Position) bool {
@@ -104,15 +222,17 @@ func (g *Guard) FindUniquePathPositions() map[Position]int {
 	log.Println("computing unique positions in path...")
 	path := map[Position]int{}
 	for _, visit := range g.History {
-		if path[visit] == 0 {
-			path[visit] += 1
+		if path[visit.Position] == 0 {
+			path[visit.Position] += 1
 		}
 	}
 	return path
 }
 
 func (g *Guard) Travel(world map[Position]string) {
+
 	// find the next square based on direction facing
+
 	nextPosition := Position{
 		X: g.Position.X + g.Facing.X,
 		Y: g.Position.Y + g.Facing.Y,
@@ -121,16 +241,37 @@ func (g *Guard) Travel(world map[Position]string) {
 	// check whether the next position is clear or obstacle
 	nextPositionStatus := world[nextPosition]
 
+	// // before we do anything, pretend there is an obstacle in front of us and we have to turn right now -- then check if that path leads us back to our path history
+	// anchor := StepInformation{
+	// 	Position:  g.Position,
+	// 	Direction: g.Facing,
+	// }
+
+	// cloneHist := []StepInformation{}
+	// _ = copy(g.History, cloneHist)
+	// clone := Guard{
+	// 	Position: g.Position,
+	// 	History:  cloneHist,
+	// 	Facing:   g.Facing,
+	// }
+
+	// clone.Facing.Rotate90()
+	// clone.Travel(world)
+
 	// if next position is clear ("."), move into that position
 	if nextPositionStatus == "." {
-		log.Printf("guard moving to %v", nextPosition)
-		g.History = append(g.History, g.Position)
+		//log.Printf("guard moving to %v", nextPosition)
+		g.History = append(g.History, StepInformation{
+			Position:  g.Position,
+			Direction: g.Facing,
+		})
 		g.Position = nextPosition
 	}
 	// if next position is obstacle ("#"), turn right ninety degrees and start over
 	if nextPositionStatus == "#" {
 		g.Facing = g.Facing.Rotate90()
 	}
+
 }
 
 func (d *Direction) Rotate90() Direction {
--- go.mod
+++ go.mod
@@ -3,8 +3,19 @@ module github.com/uncleBlobby/aoc2024
 go 1.23.1
 
 require (
-	github.com/fatih/color v1.18.0 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/hajimehoshi/ebiten v1.12.12
+	github.com/hajimehoshi/ebiten/v2 v2.8.5
+)
+
+require (
+	github.com/ebitengine/gomobile v0.0.0-20240911145611-4856209ac325 // indirect
+	github.com/ebitengine/hideconsole v1.0.0 // indirect
+	github.com/ebitengine/purego v0.8.0 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20200707082815-5321531c36a2 // indirect
+	github.com/jezek/xgb v1.1.1 // indirect
+	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
+	golang.org/x/image v0.20.0 // indirect
+	golang.org/x/mobile v0.0.0-20210208171126-f462b3930c8f // indirect
+	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
 )

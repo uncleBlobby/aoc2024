521ba8987b6d6d3033a8bd7bdecb92efc66954d1
Dustin Christianson <dc.christianson@gmail.com>
2024-12-11 20:24:56 -0600

    finished day 11 part1

--- day11/main.go
+++ day11/main.go
@@ -7,6 +7,7 @@ import (
 	"slices"
 	"strconv"
 	"strings"
+	"time"
 
 	daily "github.com/uncleBlobby/aoc2024/internal"
 )
@@ -14,7 +15,7 @@ import (
 func main() {
 	fmt.Println("Hello, aoc2024 day 11!")
 
-	input := daily.GetInputFromFile("testdata")
+	input := daily.GetInputFromFile("input")
 
 	scanner := bufio.NewScanner(input)
 
@@ -35,10 +36,13 @@ func main() {
 	}
 
 	fmt.Println(startInput)
-	stones := Blink(startInput)
-	stones = Blink(stones)
-	stones = Blink(stones)
 
+	blinkCount := 75
+	dupeCount := 0
+	Part2(startInput, blinkCount, dupeCount)
+
+	// fmt.Printf("Result after blinking %d times:\n%v\n", blinkCount, result)
+	// fmt.Printf("Final count of stones: %d\n", len(result))
 	// testSampleArray := []int{1, 2, 3, 4, 5}
 
 	// fmt.Println(testSampleArray)
@@ -48,21 +52,66 @@ func main() {
 
 }
 
+func Part2(inputArray []int, X int, dupeStoneCount int) {
+
+	for i := 0; i < X; i++ {
+		// fmt.Printf("%d / %d\n", i, X)
+		// fmt.Println(inputArray)
+		// startLen := len(inputArray)
+		inputArray = Blink(inputArray)
+		startLen := len(inputArray)
+		slices.Sort(inputArray)
+		inputArray := slices.Compact(inputArray)
+		endLen := len(inputArray)
+		dupeStoneCount += startLen - endLen
+		fmt.Printf("current stone count: %d\n", len(inputArray)+dupeStoneCount)
+	}
+
+	fmt.Printf("Total stone count after %d blinks: %d\n", X, len(inputArray)+dupeStoneCount)
+}
+
+func BlinkXTimes(inputArray []int, X int) []int {
+	fmt.Printf("Blinking %d times...\n", X)
+	for i := 0; i < X; i++ {
+		startTime := time.Now()
+		inputArray = Blink(inputArray)
+		duration := time.Since(startTime)
+		fmt.Printf("%d / %d -- %d s\n", i, X, duration/1000000000)
+	}
+
+	return inputArray
+}
+
 func Blink(inputArray []int) []int {
+	// fmt.Printf("Preparing output array of length: %d\n", len(inputArray))
 	output := make([]int, len(inputArray))
 
 	for i := 0; i < len(inputArray); i++ {
+		output[i] = inputArray[i]
+	}
+
+	slicedIn := 0
+
+	for i := 0; i < len(inputArray); i++ {
+		// fmt.Printf("Processing element [%d]: %d\n", i, inputArray[i])
 		resultElem := ProcessStone(inputArray[i])
 		if len(resultElem) == 1 {
-			output[i] = resultElem[0]
+			// fmt.Printf("Inserting [%d] at output position [%d]\n", resultElem[0], i+slicedIn)
+			output[i+slicedIn] = resultElem[0]
 		}
 		if len(resultElem) > 1 {
-			output[i] = resultElem[0]
-			output = slices.Insert(output, i+1, resultElem[1])
-			i++
+			// fmt.Printf("Inserting [%d] at output position [%d], and [%d] at output position [%d]\n", resultElem[0], i, resultElem[1], i+1)
+			output[i+slicedIn] = resultElem[0]
+			output = slices.Insert(output, i+slicedIn+1, resultElem[1])
+			slicedIn += 1
+			// i++
+			// inputArray = output
 		}
+		// fmt.Println("Current output:")
+		// fmt.Println(output)
 	}
-	fmt.Println(output)
+	// fmt.Println("Final output:")
+	// fmt.Println(output)
 	return output
 
 	// for ind, elem := range inputArray {
@@ -91,13 +140,17 @@ func Blink(inputArray []int) []int {
 func ProcessStone(inputValue int) []int {
 	one, done := RuleOne(inputValue)
 	if one {
+		// fmt.Printf("Triggered RuleOne on inputValue [%d], result: [%d]\n", inputValue, done)
 		return []int{done}
 	}
 	two, done2 := RuleTwo(inputValue)
 	if two {
+
+		// fmt.Printf("Triggered RuleTwo on inputValue [%d], result: [%d], [%d]\n", inputValue, done2[0], done2[1])
 		return []int{done2[0], done2[1]}
 	}
 	three := RuleThree(inputValue)
+	// fmt.Printf("Triggered RuleThree on inputValue [%d], result: [%d]\n", inputValue, three)
 	return []int{three}
 }
 
